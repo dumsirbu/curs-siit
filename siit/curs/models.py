70b621976fa00e14fd0c72aeefd94358bfbb1d18
Andrei Coman <andrei.coman@scoalainformala.ro>
2022-06-14 20:50:17 +0300

    Add student model

--- siit/curs/models.py
+++ siit/curs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    nume = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefon = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.nume} {self.email}"
