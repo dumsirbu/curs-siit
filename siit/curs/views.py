9670a3ae4d534296ec3274b292f4fd52f2ecc829
Andrei Coman <andrei.coman@scoalainformala.ro>
2022-06-14 20:55:57 +0300

    Show static list of students

--- siit/curs/views.py
+++ siit/curs/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello World!")
+
+def show_students(request):
+    lista_studenti = ["Dorel", "Georgel"]
+    return HttpResponse(lista_studenti)
--- siit/siit/urls.py
+++ siit/siit/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from curs.views import hello_world
+from curs.views import hello_world, show_students
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('salut', hello_world)
+    path('salut', hello_world),
+    path('studenti', show_students)
 ]
 
 
