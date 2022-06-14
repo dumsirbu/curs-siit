e6130254f982f787537b0eaa92170d6d78a80149
Andrei Coman <andrei.coman@scoalainformala.ro>
2022-06-14 21:01:38 +0300

    Show students view

--- siit/curs/views.py
+++ siit/curs/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Student
+
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello World!")
 
 def show_students(request):
-    lista_studenti = ["Dorel", "Georgel"]
-    return HttpResponse(lista_studenti)
+    lista_studenti = Student.objects.all()
+    studenti = "<br/>".join(s.nume for s in lista_studenti)
+    return HttpResponse(studenti)
