d4a8c4e942f08eaa56075744c4d4e3c662b72050
Andrei Coman <comandrei@gmail.com>
2022-06-16 19:01:15 +0300

    Add curs and change student model

--- siit/curs/models.py
+++ siit/curs/models.py
@@ -4,8 +4,14 @@ from django.db import models
 
 class Student(models.Model):
     nume = models.CharField(max_length=50)
+    prenume = models.CharField(max_length=50, null=True)
+    an = models.IntegerField()
     email = models.EmailField()
     telefon = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return f"{self.nume} {self.email}"
+
+class Curs(models.Model):
+    nume = models.CharField(max_length=50)
+    an = models.IntegerField()
