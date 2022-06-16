54124bb1487582f5f9add3abfe92152d43e21a7f
Andrei Coman <comandrei@gmail.com>
2022-06-16 20:08:32 +0300

    Filter by year

--- siit/curs/views.py
+++ siit/curs/views.py
@@ -8,7 +8,8 @@ def hello_world(request):
     return HttpResponse("Hello World!")
 
 def show_students(request):
-    lista_studenti = Student.objects.all()
+    an_cerut = request.GET['an']
+    lista_studenti = Student.objects.filter(an=an_cerut)
     context = {
         'studenti': lista_studenti,
         'mesaj': 'Salut'
