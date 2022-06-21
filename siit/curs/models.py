2274c0a6485e4b497552102f2fe5aba7b1ac1ee1
Andrei Coman <comandrei@gmail.com>
2022-06-21 18:57:22 +0300

    Unique index

--- siit/curs/models.py
+++ siit/curs/models.py
@@ -6,11 +6,14 @@ class Student(models.Model):
     nume = models.CharField(max_length=50, db_index=True)
     prenume = models.CharField(max_length=50, null=True)
     an = models.IntegerField()
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     telefon = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return f"{self.nume} {self.email}"
+
+    def afiseaza_studenti(self):
+        pass
 
 class Curs(models.Model):
     nume = models.CharField(max_length=50)
