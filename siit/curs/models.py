353df420b9e51f2170c067520d40a1c057609d53
Andrei Coman <comandrei@gmail.com>
2022-06-21 19:05:21 +0300

    Nume/prenume unique_together

--- siit/curs/models.py
+++ siit/curs/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    class Meta:
+        unique_together = ("nume", "prenume")
+
     nume = models.CharField(max_length=50, db_index=True)
     prenume = models.CharField(max_length=50, null=True)
     an = models.IntegerField(default=1)
