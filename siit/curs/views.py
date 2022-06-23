7c49fe749ae00008c814cb1619c39789b49861f0
Andrei Coman <comandrei@gmail.com>
2022-06-23 19:08:50 +0300

    Handle no year

--- siit/curs/views.py
+++ siit/curs/views.py
@@ -8,8 +8,12 @@ def hello_world(request):
     return HttpResponse("Hello World!")
 
 def show_students(request):
-    an_cerut = int(request.GET['an'])
-    lista_studenti = Student.objects.filter(an=an_cerut)
+    try:
+        an_cerut = int(request.GET['an'])
+        lista_studenti = Student.objects.filter(an=an_cerut)
+    except KeyError:
+        lista_studenti = Student.objects.all()
+
     context = {
         'studenti': lista_studenti,
         'mesaj': 'Salut'
