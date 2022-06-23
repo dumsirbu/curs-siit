d754baa848b3bb2c643ecfffade6ac0a9bc2423e
Andrei Coman <comandrei@gmail.com>
2022-06-23 19:32:18 +0300

    lte example

--- siit/curs/views.py
+++ siit/curs/views.py
@@ -7,15 +7,19 @@ from .models import Student
 def hello_world(request):
     return HttpResponse("Hello World!")
 
+
 def show_students(request):
     try:
         an_cerut = int(request.GET['an'])
-        lista_studenti = Student.objects.filter(an=an_cerut)
+        lista_studenti = Student.objects.filter(an__lte=an_cerut)
     except KeyError:
         lista_studenti = Student.objects.all()
-
     context = {
         'studenti': lista_studenti,
         'mesaj': 'Salut'
     }
     return render(request, "list_students.html", context)
+
+
+def filter(*args, **kwargs):
+    pass
