758b9f41cc91e08a43e6e65109da0e1be7312dab
Andrei Coman <comandrei@gmail.com>
2022-06-30 20:02:58 +0300

    Switch to text area for message

--- siit/curs/forms.py
+++ siit/curs/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     nume = forms.CharField(max_length=10, required=False, label="Numele tau")
     email = forms.EmailField()
-    text = forms.CharField(max_length=2048)
+    text = forms.CharField(max_length=2048, widget=forms.Textarea)
     
