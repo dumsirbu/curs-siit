90ba219ce974fea958355e632b68e5bc4d977012
Andrei Coman <comandrei@gmail.com>
2022-07-12 20:49:32 +0300

    Filter based on GET parameters

--- siit/curs/viewsets.py
+++ siit/curs/viewsets.py
@@ -6,3 +6,10 @@ from .serializers import StudentSerializer
 class StudentViewSet(ModelViewSet):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'an' in self.request.GET:
+            an = int(self.request.GET['an'])
+            return qs.filter(an=an)
+        return qs
