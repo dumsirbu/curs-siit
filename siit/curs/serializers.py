5cb01c09b26afd514feb6f56dc5e78659a205ab1
Andrei Coman <comandrei@gmail.com>
2022-07-14 19:33:42 +0300

    Custom fields in serializer

--- siit/curs/serializers.py
+++ siit/curs/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from django.utils import timezone
+
+from rest_framework.serializers import HyperlinkedModelSerializer, SerializerMethodField
 
 from .models import Curs, Student
 
@@ -12,4 +14,13 @@ class StudentSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Student
         fields = "__all__"
+
     cursuri = CursSerializer(many=True)
+    timestamp = SerializerMethodField()
+    full_name = SerializerMethodField()
+
+    def get_timestamp(self, arg):
+        return timezone.now()
+
+    def get_full_name(self, obj):
+        return f"{obj.nume} {obj.prenume}"
