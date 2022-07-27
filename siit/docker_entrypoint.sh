9360a47e3a9d52c5085881dc8d45b9e0155baf6c
Andrei Coman <comandrei@gmail.com>
2022-07-27 19:49:02 +0300

    Add UWSGI

--- siit/docker_entrypoint.sh
+++ siit/docker_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py createsuperuser --noinput --username admin1
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+uwsgi --http :8000 --wsgi-file siit/wsgi.py 
