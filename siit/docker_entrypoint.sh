31f2469e00d17ff1adb2d93275faf92ea6f15e4e
Andrei Coman <comandrei@gmail.com>
2022-08-02 19:39:20 +0300

    Multi mode images

--- siit/docker_entrypoint.sh
+++ siit/docker_entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py createsuperuser --noinput --username admin1
-#python manage.py runserver 0.0.0.0:8000
-uwsgi --http :8000 --wsgi-file siit/wsgi.py 
+case $1 in
+    local-server)
+        python manage.py runserver 0.0.0.0:8000
+    ;;
+    production-server)
+        uwsgi --http :8000 --wsgi-file siit/wsgi.py 
+    ;;
+    *)
+        $1
+    ;;
+esac
